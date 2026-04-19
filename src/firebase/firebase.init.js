ad1a0294b8a0dfa274d1e05eb02b0141f39c63a2
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-04-20 01:19:22 +0600

    first commit

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMkDV8VZVcjl7v20oZMmXk5tuCp8BZenY",
+  authDomain: "private-route-with-auth-react.firebaseapp.com",
+  projectId: "private-route-with-auth-react",
+  storageBucket: "private-route-with-auth-react.firebasestorage.app",
+  messagingSenderId: "255306631812",
+  appId: "1:255306631812:web:c6d426a02dfa12756290d1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <p>This is Register</p>
+    </div>
+  );
+};
+
+export default Register;
